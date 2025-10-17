e4c6ba23db15eafbcc3f3d0c0f89171e930443ed
Azlan Jalil <azlanjalil543@gmail.com>
2025-10-17 23:32:25 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <header className="text-center">
+        <h1 className="text-5xl text-red-600">Header</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
