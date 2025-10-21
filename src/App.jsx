199116aecd1f95c4477821a536424aa6ff11eb4f
Azlan Jalil <azlanjalil543@gmail.com>
2025-10-21 22:44:40 +0500

    Counter State

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <header className="text-center">
+      {/* <header className="text-center">
         <h1 className="text-5xl text-red-600">Header</h1>
-      </header>
+      </header> */}
+      <main className="flex flex-col gap-5 items-center justify-center h-dvh">
+        <h2 className="text-4xl">Count: {count}</h2>
+        <button
+          onClick={() => setCount(count + 1)}
+          className="bg-blue-500 text-white px-4 py-2 rounded w-fit"
+        >
+          Increment
+        </button>
+      </main>
     </>
   );
 }
