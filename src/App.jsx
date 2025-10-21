b4bc6675d72ef3b53dc40f2d9112b7d02458a906
Azlan Jalil <azlanjalil543@gmail.com>
2025-10-21 23:39:13 +0500

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
-import "./App.css";
-function App() {
-  const [count, setCount] = useState(0);
+import Counter from "./components/Counter";
+
+const App = () => {
   return (
-    <>
-      {/* <header className="text-center">
-        <h1 className="text-5xl text-red-600">Header</h1>
-      </header> */}
-      <main className="flex flex-col gap-5 items-center justify-center h-dvh">
-        <h2 className="text-4xl">Count: {count}</h2>
-        <button
-          onClick={() => setCount(count + 1)}
-          className="bg-blue-500 text-white px-4 py-2 rounded w-fit"
-        >
-          Increment
-        </button>
-      </main>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-blue-50 font-sans">
+      <Counter />
+    </div>
   );
-}
+};
 
 export default App;
