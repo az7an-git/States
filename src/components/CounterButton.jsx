79b49ab73c65d71580655a1be915094107dcb6f5
Azlan Jalil <azlanjalil543@gmail.com>
2025-10-22 13:52:07 +0500

    again

--- src/components/CounterButton.jsx
+++ src/components/CounterButton.jsx
@@ -1,12 +1,12 @@
-// const CounterButton = ({ onClick, label, styleClass }) => {
-//   return (
-//     <button
-//       onClick={onClick}
-//       className={`${styleClass} px-4 py-2 rounded-lg font-semibold text-sm shadow-md hover:scale-105 transition-transform duration-200`}
-//     >
-//       {label}
-//     </button>
-//   );
-// };
+const CounterButton = ({ onClick, label, styleClass }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${styleClass} px-4 py-2 rounded-lg font-semibold text-sm shadow-md hover:scale-105 transition-transform duration-200`}
+    >
+      {label}
+    </button>
+  );
+};
 
-// export default CounterButton;
+export default CounterButton;
