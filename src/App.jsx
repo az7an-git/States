c7a5b9036390589a3c9cd17cbcd18f5b989fa946
Azlan Jalil <azlanjalil543@gmail.com>
2025-10-22 15:26:13 +0500

    fix ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Counter from "./components/Counter";
+import Counter from "./pages/Counter";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-50 font-sans">
+    <div className="min-h-screen flex items-center justify-center bg-blue-200 font-sans">
       <Counter />
     </div>
   );
