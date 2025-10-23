cbf6e466b75732d4defbf377a6ce0cc065dd1269
Azlan Jalil <azlanjalil543@gmail.com>
2025-10-23 13:33:33 +0500

    made folder structure and new p

---
+++ ColorPicker/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl text-center">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
